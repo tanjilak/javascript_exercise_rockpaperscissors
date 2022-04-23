e1d444deb93a613e34833c45381f5b8b5c4dab00
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-04-23 01:35:43 +0000

    fixed array error

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ function computerPlay(plays){
 //take out of function for the other function to gain access
 
 //array of options
-let plays =["Rock, Paper, Scissors"]
+let plays =["rock", "paper" ,"scissors"]
 
 //random string from array
 let randomPlay = plays[Math.floor(Math.random()*plays.length)]
 
 function playRound(playerSelection, computerSelection){
-
+  if (computerSelection == "rock" && playerSelection == "rock"){
+    return "Oop"
+  }
 }
 
 const playerSelection = "rock";
-const computerSelection = plays;
+const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
