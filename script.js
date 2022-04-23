cbdd8d6080103a5ead3268a39fbbaab7f580e8bc
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-04-23 01:28:48 +0000

    took array out of function

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function computerPlay(plays){
+  //array acting as paramater for function
   
-
-  let randomPlay = plays[Math.floor(Math.random()*plays.length)]
-  console.log(randomPlay)
-
-  
+  //print out random string in console
+  console.log("Computer Selection: " + randomPlay)
 }
 
-computerPlay(["Rock", "Paper", "Scissor"])
+//take out of function for the other function to gain access
 
+//array of options
+let plays =["Rock, Paper, Scissors"]
+
+//random string from array
+let randomPlay = plays[Math.floor(Math.random()*plays.length)]
+
+function playRound(playerSelection, computerSelection){
+
+}
+
+const playerSelection = "rock";
+const computerSelection = plays;
+console.log(playRound(playerSelection, computerSelection));
